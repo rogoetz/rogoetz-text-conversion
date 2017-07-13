977e580a183f832fc130d90b011b4db16ff21b0f
rogoetz <rogoetz@ad.unc.edu>
2017-07-13 12:55:08 -0400

    added first script helloworld

---
+++ scripting/helloworld.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# say hello 
+echo " "
+echo "Hello, Rosa!"
+echo " "
+
+#give the date
+
+echo "Right now it is: `date` "
+echo " "
+echo "You are: `whoami` on `hostname` (`hostname -i`)"
+echo " "
+echo "You are in: `pwd`"
+echo " "
+echo "The files within are: `ls -lah`"
